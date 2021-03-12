8c56b687d4f7d2a36e6faf950998b1df0000582b
JanezSedeljsak <janez.sedeljsak@gmail.com>
2021-03-12 03:07:06 +0100

    generating md template with some boilerplate

--- Main.kt
+++ Main.kt
@@ -14,17 +14,64 @@ fun runTests() {
     }
 }
 
+fun runGenerateTemplate(templateName: String) {
+    val fileName = "../templates/%s.md".format(templateName)
+    val myfile = File(fileName)
+
+    var boilerPlateLines = """
+        # Markdown demo....
+        ## This is a title
+        ### This is a smaller title
+        <table>
+            <tr>
+                <th>this is a table header</th>
+                <th>another table header</th>
+            </tr>
+            <tr>
+                <td>this is a table cell</td>
+                <td>this is another cell</td>
+            </tr>
+        </table>
+        
+        - list item
+        - another list item
+        
+        <font color="green"> Some green text </font>
+        
+        ### this is an embeded code block
+        ```cpp
+        #include <iostream>
+        
+        int main() {
+            std::cout << "Hello World from C++!";
+            return 0;
+        }
+        ```
+    """.trimIndent().split("\n").toTypedArray()
+    
+
+    myfile.printWriter().use { out -> 
+        for(line in boilerPlateLines) {
+            out.println(line)
+        }
+    }
+}
+
 fun main(args: Array<String>) {
     if (args.size == 0) {
         println("Command line argument required!")
         return
     }
     when (args[0]) {
-        "test" -> {
-            runTests()
+        "test" -> runTests()
+        "generate-template" -> {
+            var templateName: String = UUID.randomUUID().toString()
+            println(args[1])
+            if (args.size > 1 && !args[1].isNullOrEmpty()) {
+                templateName = args[1]
+            }
+            runGenerateTemplate(templateName)
         }
-        else -> {
-            print("Invalid argument!")
-        }
+        else -> print("Invalid argument!")
     }
 }
--- run.bat
+++ run.bat
@@ -2,6 +2,7 @@
     if %1% == knit goto KnitToPdf
     if %1% == test goto CoreTester
     if %1% == generate-template goto GenerateTemplate
+    if %1% == edit-template goto EditTemplateOnline
     if %1% == javac goto CompileJavaClasses
     goto End
 
@@ -15,13 +16,21 @@
 
     :CoreTester
         echo Testing JSM Core....
+        if not exist tests_log mkdir tests_log
         kotlinc Main.kt -d build && cd build && kotlin MainKt test && cd..
         goto End
 
     :GenerateTemplate
         echo Generating markdown template....
+        if not exist templates mkdir templates
+        kotlinc Main.kt -d build && cd build && kotlin MainKt generate-template %2 && cd..
         goto End
-    
+
+    :EditTemplateOnline
+        :: open website for editing markdown + live preview of non executable parts
+        start "" https://stackedit.io/app#
+        goto End
+
     :CompileJavaClasses
         echo Deleting data from build...
         del /S build
